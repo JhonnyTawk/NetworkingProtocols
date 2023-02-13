f7a9a798f57d41482bbbd0081999101a853e725f
Jhonny <jhonny@Jhonnys-MacBook-Pro.local>
2023-02-13 23:37:23 +0200

    Setup Project

---
+++ NetworkingProtocols/NetworkingProtocols/NetworkingProtocols.h
@@ -0,0 +1,18 @@
+//
+//  NetworkingProtocols.h
+//  NetworkingProtocols
+//
+//  Created by Jhonny on 13/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkingProtocols.
+FOUNDATION_EXPORT double NetworkingProtocolsVersionNumber;
+
+//! Project version string for NetworkingProtocols.
+FOUNDATION_EXPORT const unsigned char NetworkingProtocolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkingProtocols/PublicHeader.h>
+
+
